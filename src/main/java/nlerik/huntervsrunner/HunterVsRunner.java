20a8fa9363361d23a06d3b0e3947131b83164d11
bartleurink <bartleurink@hotmail.com>
2023-10-13 20:10:40 +0200

    add import for groups

--- src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
+++ src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
@@ -1,15 +1,7 @@
 package nlerik.huntervsrunner;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import me.lucko.luckperms.api.LuckPerms;
-import me.lucko.luckperms.api.Node;
-import me.lucko.luckperms.api.Group;
-import me.lucko.luckperms.api.cacheddata.CachedPermissionData;
-import me.lucko.luckperms.api.context.ContextSet;
-import me.lucko.luckperms.api.context.ContextBuilder;
-import me.lucko.luckperms.api.context.ImmutableContextSet;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.RegisteredServiceProvider;
+
 
 public final class HunterVsRunner extends JavaPlugin {
 //hoientest
@@ -24,9 +16,4 @@ public final class HunterVsRunner extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    public void createGroup(String groupName) {
-        if (api == null) {
-            getLogger().severe("LuckPerms not initialized.");
-            return;
-        }
 }
