43858ac2a9193bac76fe63cd7c2505f5b727767e
erik van lohuizen <erikvlohuizen@hotmail.com>
2023-10-13 19:41:37 +0200

    Init HunterVsRunner repo

---
+++ src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
@@ -0,0 +1,17 @@
+package nlerik.huntervsrunner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HunterVsRunner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
