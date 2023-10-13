259ae0712fe7fd4f86aa6f3cac7c24674fb71921
erik van lohuizen <erikvlohuizen@hotmail.com>
2023-10-13 21:18:06 +0200

    added player eventhandlers

--- src/main/java/nlerik/huntervsrunner/GameListener.java
+++ src/main/java/nlerik/huntervsrunner/GameListener.java
@@ -1,8 +1,12 @@
 package nlerik.huntervsrunner;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class GameListener implements Listener {
     private final GameManager gameManager;
@@ -14,6 +18,22 @@ public class GameListener implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         // Logic to handle player movement event
+        //if player is runner update compasses in inventories of hunters
 
+        if (event.getPlayer().equals(gameManager.getRunner())) {
+            for (Player hunter : gameManager.getHunters()) {
+                hunter.setCompassTarget(event.getPlayer().getLocation());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        // Logic to handle player death event
+        //if player is hunter give a new compass
+        if (gameManager.getHunters().contains(event.getPlayer())) {
+            //add compass to inventory
+            event.getPlayer().getInventory().addItem(new ItemStack(Material.COMPASS));
+        }
     }
 }
