6be8127c6e24ab4a021682d8167661041bd3fde2
bartleurink <bartleurink@hotmail.com>
2023-10-13 19:58:22 +0200

    test op line 13

--- src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
+++ src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
@@ -3,7 +3,7 @@ package nlerik.huntervsrunner;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HunterVsRunner extends JavaPlugin {
-
+//hoi
     @Override
     public void onEnable() {
         // Plugin startup logic
