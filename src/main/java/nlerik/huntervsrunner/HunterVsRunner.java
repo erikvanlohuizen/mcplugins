20ad2d94f597a7f04507190022bbe5ff1d606bc1
erik van lohuizen <erikvlohuizen@hotmail.com>
2023-10-13 20:23:31 +0200

    todo's

--- src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
+++ src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
@@ -15,3 +15,20 @@ public final class HunterVsRunner extends JavaPlugin {
         // Plugin shutdown logic
     }
 }
+
+// Must haves
+// Commands: join hunter, join runner, start game, stop game
+// Start game: runner: speed 30sec, hunter: blindness 30sec, inv clear, max health, max food
+// Join hunter: add to hunter team
+// Join runner: add to runner team
+// Stop game (if game is running): reset teams
+
+// Game logic
+// compass points to runner in every dimension or last known location in other dimension
+
+// Nice to haves
+// perks for hunter and runner
+// multiple hunters
+// nicer message visuals
+// game ends automatically if runner dies or slays dragon
+// commands clickable in chat
