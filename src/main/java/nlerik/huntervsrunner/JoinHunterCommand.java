3dbc50b6059d196224221820b252dd8abefac375
bartleurink <bartleurink@hotmail.com>
2024-02-02 21:07:50 +0100

    Merge remote-tracking branch 'origin/master' Title message gemaakt voor joinen hunter en runner

--- src/main/java/nlerik/huntervsrunner/JoinHunterCommand.java
+++ src/main/java/nlerik/huntervsrunner/JoinHunterCommand.java
@@ -1,9 +1,11 @@
 package nlerik.huntervsrunner;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
 
 public class JoinHunterCommand implements CommandExecutor {
     private final GameManager gameManager;
@@ -14,9 +16,22 @@ public class JoinHunterCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
         Player player = (Player) sender;
         gameManager.AddHunter(player);
         player.sendMessage("You have chosen Hunter!");
+
+        // Send a colored title when the player chooses Hunter
+        String title = ChatColor.GREEN + "Welcome " + ChatColor.YELLOW + "Hunter!";
+        String subtitle = ChatColor.AQUA + "Prepare for the hunt!";
+
+        // Send a colored title with fade-in, display, and fade-out times
+        player.sendTitle(title, subtitle, 10, 70, 20);
+
         // Logic to handle joining the hunter team
         return true;
     }
--- src/main/java/nlerik/huntervsrunner/JoinRunnerCommand.java
+++ src/main/java/nlerik/huntervsrunner/JoinRunnerCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 
 public class JoinRunnerCommand implements CommandExecutor {
     private final GameManager gameManager;
@@ -14,10 +15,23 @@ public class JoinRunnerCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
         Player player = (Player) sender;
         gameManager.SetRunner(player);
         player.sendMessage("You have chosen Runner!");
-        // Logic to handle joining the hunter team
+
+        // Send a colored title when the player chooses Runner
+        String title = ChatColor.BLUE + "Welcome " + ChatColor.GREEN + "Runner!";
+        String subtitle = ChatColor.GOLD + "Prepare for the race!";
+
+        // Send a colored title with fade-in, display, and fade-out times
+        player.sendTitle(title, subtitle, 10, 70, 20);
+
+        // Logic to handle joining the runner team
         return true;
     }
 }
