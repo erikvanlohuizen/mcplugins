61bfb7cf031072c26bc8ece4ced48e4fa21528e8
bartleurink <bartleurink@hotmail.com>
2024-02-07 22:05:22 +0100

    Menu veranderd naar een menu in een book

--- src/main/java/nlerik/huntervsrunner/GameMenuCommand.java
+++ src/main/java/nlerik/huntervsrunner/GameMenuCommand.java
@@ -1,16 +1,21 @@
 package nlerik.huntervsrunner;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
@@ -26,8 +31,7 @@ public class GameMenuCommand implements CommandExecutor, Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
-            Player player = (Player) sender;
-            giveMenuBook(player);
+            giveMenuBook((Player) sender);
             return true;
         } else {
             sender.sendMessage(ChatColor.RED + "Only players can execute this command!");
@@ -36,25 +40,28 @@ public class GameMenuCommand implements CommandExecutor, Listener {
     }
 
     private void giveMenuBook(Player player) {
-        ItemStack menuBook = createMenuBook(player);
+        ItemStack menuBook = createMenuBook();
         player.getInventory().addItem(menuBook);
         player.sendMessage(ChatColor.GREEN + "You received the game menu book!");
     }
 
-    private ItemStack createMenuBook(Player player) {
+    private ItemStack createMenuBook() {
         ItemStack menuBook = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta bookMeta = (BookMeta) menuBook.getItemMeta();
         bookMeta.setTitle("Game Menu");
-        bookMeta.setAuthor(player.getName());
+        bookMeta.setAuthor("Server");
 
         TextComponent.Builder builder = Component.text().color(NamedTextColor.DARK_RED)
                 .content("Game Menu\nClick the options below to perform actions:\n\n")
-                .append(Component.text("[Join the hunters]").color(NamedTextColor.DARK_BLUE).clickEvent(
-                        ClickEvent.runCommand("/joinhunter")))
-                .append(Component.text("\n[Join the runners]").color(NamedTextColor.DARK_PURPLE).clickEvent(
-                        ClickEvent.runCommand("/joinrunner")))
-                .append(Component.text("\n[Start the game]").color(NamedTextColor.DARK_GREEN).clickEvent(
-                        ClickEvent.runCommand("/startgame")));
+                .append(Component.text("[Join the hunters]").color(NamedTextColor.DARK_BLUE)
+                        .clickEvent(ClickEvent.runCommand("/joinhunter"))
+                        .hoverEvent(Component.text("Click to join the hunters").color(NamedTextColor.BLUE)))
+                .append(Component.text("\n[Join the runners]").color(NamedTextColor.DARK_PURPLE)
+                        .clickEvent(ClickEvent.runCommand("/joinrunner"))
+                        .hoverEvent(Component.text("Click to join the runners").color(NamedTextColor.DARK_PURPLE)))
+                .append(Component.text("\n[Start the game]").color(NamedTextColor.DARK_GREEN)
+                        .clickEvent(ClickEvent.runCommand("/startgame"))
+                        .hoverEvent(Component.text("Click to start the game").color(NamedTextColor.GREEN)));
 
         bookMeta.addPages(builder.build());
         menuBook.setItemMeta(bookMeta);
