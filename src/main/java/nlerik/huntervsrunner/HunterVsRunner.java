453c15510ef2424aa81c55c18741699e44c4e9a2
bartleurink <bartleurink@hotmail.com>
2026-02-17 14:18:13 +0100

    Stop command toegevoegd

--- src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
+++ src/main/java/nlerik/huntervsrunner/HunterVsRunner.java
@@ -20,6 +20,8 @@ public final class HunterVsRunner extends JavaPlugin {
         getCommand("joinrunner").setExecutor(new JoinRunnerCommand(gameManager));
         getCommand("startgame").setExecutor(new StartCommand(gameManager));
         getCommand("huntervsrunner").setExecutor(new GameMenuCommand(gameManager));
+        getCommand("stopgame").setExecutor(new StopGameCommand(gameManager));
+
 
 
         Bukkit.getServer().getPluginManager().registerEvents(new GameListener(gameManager), this);
---
+++ src/main/java/nlerik/huntervsrunner/StopCommand.java
@@ -0,0 +1,41 @@
+package nlerik.huntervsrunner;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class StopGameCommand implements CommandExecutor {
+
+    private final GameManager gameManager;
+
+    public StopGameCommand(GameManager gameManager) {
+        this.gameManager = gameManager;
+        System.out.println("StopGameCommand loaded!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player player) {
+            if (gameManager.IsGameRunning()) {
+                gameManager.stopGame(false);
+                Bukkit.broadcastMessage(ChatColor.RED + "The game has been stopped by " + player.getName() + "!");
+            } else {
+                player.sendMessage(ChatColor.YELLOW + "No game is currently running.");
+            }
+            return true;
+        } else {
+            // Console kan ook stoppen
+            if (gameManager.IsGameRunning()) {
+                gameManager.stopGame(false);
+                Bukkit.getServer().broadcastMessage(ChatColor.RED + "The game has been stopped by the console!");
+            } else {
+                System.out.println("No game is currently running.");
+            }
+            return true;
+        }
+    }
+}
